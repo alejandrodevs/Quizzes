f588655b63d28acb7756a87ae58c44eadc2edb4e
Alejandro Gutiérrez <alejandro.gutierrez@agilekoding.com>
2013-04-01 13:13:30 -0600

    Refactorizing the next_highest.rb file

--- ruby/next_highest/next_highest.rb
+++ ruby/next_highest/next_highest.rb
@@ -2,23 +2,22 @@ module NextHighest
 
   def next_highest
     value = self.to_s.scan(/\d/).reverse
-    upper = 1
 
-    while upper < value.length
+    (1...value.length).each do |upper|
       (0..upper).each do |i|
         (i.next..upper).each do |j|
-          return sort_value(value, i, j).reverse.join.to_i if value[i] > value[j]
+          return sort_value(value, i, j) if value[i] > value[j]
         end
       end
-      upper = upper.next
     end
-    return value.reverse.join.to_i
+
+    self
   end
 
   def sort_value value, current, after
     value[after], value[current] = value[current], value[after]
     value[0..after.pred] = value.slice(0, after).sort.reverse
-    value
+    value.reverse.join.to_i
   end
 
 end
