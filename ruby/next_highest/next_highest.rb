9c33d0657aa62e4081e2e57a048917cf5e4595f0
Alejandro Gutiérrez <alejandro.gutierrez@agilekoding.com>
2013-04-01 12:17:23 -0600

    Ruby programming challenges

---
+++ ruby/next_highest/next_highest.rb
@@ -0,0 +1,35 @@
+module NextHighest
+
+  def self.included(base)
+    base.send :include, InstanceMethods
+  end
+
+  # Instance methods
+  # =============================================================
+  module InstanceMethods
+    def next_highest
+      value = self.to_s.scan(/\d/).reverse
+      upper = 1
+
+      while upper < value.length
+        (0..upper).each do |i|
+          (i.next..upper).each do |j|
+            return sort_value(value, i, j).reverse.join.to_i if value[i] > value[j]
+          end
+        end
+        upper = upper.next
+      end
+      return value.reverse.join.to_i
+    end
+
+    def sort_value value, current, after
+      value[after], value[current] = value[current], value[after]
+      value[0..after.pred] = value.slice(0, after).sort.reverse
+      value
+    end
+  end
+
+end
+
+Fixnum.send :include, NextHighest
+Bignum.send :include, NextHighest
---
+++ ruby/next_highest/next_highest_test.rb
@@ -0,0 +1,21 @@
+require 'test/unit'
+require './next_highest'
+
+class NextHighestTest < Test::Unit::TestCase
+
+  #def simple_test
+    #assert_equal(4, 4.next_highest)
+    #assert_equal(71, 17.next_highest)
+    #assert_equal(71, 71.next_highest)
+    #assert_equal(318, 183.next_highest)
+    #assert_equal(381, 318.next_highest)
+    #assert_equal(813, 381.next_highest)
+    #assert_equal(831, 813.next_highest)
+    #assert_equal(1324, 1243.next_highest)
+    #assert_equal(4321, 4321.next_highest)
+    #assert_equal(38627, 38276.next_highest)
+    #assert_equal(62378, 38762.next_highest)
+  #end
+
+end
+
